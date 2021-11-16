679c146c0b3a7e17b23a685fd0ef05f47bc64e06
Steven Crutchfield <stevccru@icloud.com>
2021-11-16 07:28:49 -0500

    added material UI

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from "./views/Home";
+import DropdownSelect from "./components/DropdownSelect";
+import Header from "./components/Header"
+import Page from "./components/Page"
 
 function App() {
   return (
     <div className="App">
+      
       <Home />
     </div>
   );
