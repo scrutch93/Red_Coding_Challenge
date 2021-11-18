49b395a24773f23d72c74dcdb67860c22e3d98d3
Steven Crutchfield <stevccru@icloud.com>
2021-11-17 19:26:19 -0500

    added updated mui module

--- Frontend/src/views/Home/index.tsx
+++ Frontend/src/views/Home/index.tsx
@@ -4,7 +4,7 @@ import DropdownSelect from "../../components/DropdownSelect";
 import Header from "../../components/Header"
 import Container from '@material-ui/core/Container';
 import SearchBar from "material-ui-search-bar"
-import { flexbox } from '@mui/system';
+import Box from '@mui/material/Box'
 
 export default function Home(){
     
@@ -16,7 +16,7 @@ export default function Home(){
       <DropdownSelect  options={[{label: 'Bobs Burgers', value: 1}, {label: 'Wendys', value: 1},{label: 'Amazon', value: 1}, {label: 'Coca-Cola', value: 1},{label: 'Dunder Mifflin', value: 1}]} placeholder ="ok" onSelectOption ={()=>{}} value= {{label: 'Customers', value: 1}} />
       <DropdownSelect  options={[{label: 'Standard', value: 1}, {label: 'SaleOrder', value: 1},{label: 'PurchaseOrder', value: 1}, {label: 'TransferOrder', value: 1},{label: 'ReturnOrder', value: 1}]} placeholder ="ok" onSelectOption ={()=>{}} value= {{label: 'Order Type', value: 1}} />
       
-    </div>
+        </div>
 
 
 
