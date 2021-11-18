234389dbf075c927e98d92aa6e3a679c11b318bf
Steven Crutchfield <stevccru@icloud.com>
2021-11-18 11:49:19 -0500

    updated grid

--- Frontend/src/components/Table/index.tsx
+++ Frontend/src/components/Table/index.tsx
@@ -2,49 +2,49 @@ import * as React from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'id', headerName: 'Order ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'creationDate',
+    headerName: 'Creation Date',
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: 'createdBy',
+    headerName: 'Created By',
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'orderType',
+    headerName: 'Order Type',
     type: 'number',
     width: 110,
     editable: true,
   },
   {
     field: 'fullName',
-    headerName: 'Full name',
+    headerName: 'Customer',
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
     width: 160,
     valueGetter: (params: GridValueGetterParams) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
+      `${params.getValue(params.id, 'creationDate') || ''} ${
+        params.getValue(params.id, 'createdBy') || ''
       }`,
   },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, creationDate: 'Snow', createdBy: 'Jon', orderType: 35 },
+  { id: 2, creationDate: 'Lannister', createdBy: 'Cersei', orderType: 42 },
+  { id: 3, creationDate: 'Lannister', createdBy: 'Jaime', orderType: 45 },
+  { id: 4, creationDate: 'Stark', createdBy: 'Arya', orderType: 16 },
+  { id: 5, creationDate: 'Targaryen', createdBy: 'Daenerys', orderType: null },
+  { id: 6, creationDate: 'Melisandre', createdBy: null, orderType: 150 },
+  { id: 7, creationDate: 'Clifford', createdBy: 'Ferrara', orderType: 44 },
+  { id: 8, creationDate: 'Frances', createdBy: 'Rossini', orderType: 36 },
+  { id: 9, creationDate: 'Roxie', createdBy: 'Harvey', orderType: 65 },
 ];
 
 export default function Table() {
@@ -53,8 +53,8 @@ export default function Table() {
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={15}
+        rowsPerPageOptions={[15]}
         checkboxSelection
         disableSelectionOnClick
       />
