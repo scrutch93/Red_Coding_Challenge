752a805b9ee7a0b7f74f6777d009be11b5f5fd4f
Steven Crutchfield <stevccru@icloud.com>
2021-11-20 10:48:02 -0500

    updated changes with new approach

--- Frontend/src/components/Orders/index.tsx
+++ Frontend/src/components/Orders/index.tsx
@@ -1,39 +1,39 @@
 import React,{Component} from 'react';
 import {variables} from '../API/variables';
 
+export class Orders extends React.Component <any, any>{
 
-export class Orders extends Component <any, any> {
-
-    constructor(props: any){
+    constructor(props){
         super(props);
 
         this.state={
-            modalTitle: "",
-            orderId: 0,
-            orderType:[],
-          
-            orderIdFilter:"",
+            orders:[],
+            modalTitle:"",
             orderType:"",
-            ordersWithoutFilter:[]
+            orderID:0,
 
+            orderIDFilter:"",
+            orderTypeFilter:"",
+            ordersWithoutFilter:[]
+        }
     }
 
     FilterFn(){
-        var orderIdFilter=this.state.orderIdFilter;
+        var orderIDFilter=this.state.orderIDFilter;
         var orderTypeFilter = this.state.orderTypeFilter;
 
         var filteredData=this.state.ordersWithoutFilter.filter(
             function(el){
-                return el.orderId.toString().toLowerCase().includes(
-                    orderIdFilter.toString().trim().toLowerCase()
+                return el.orderID.toString().toLowerCase().includes(
+                    orderIDFilter.toString().trim().toLowerCase()
                 )&&
-                el.orderType.toString().toLowerCase().includes(
+                el.ordertype.toString().toLowerCase().includes(
                     orderTypeFilter.toString().trim().toLowerCase()
                 )
             }
         );
 
-        this.setState({Orders:filteredData});
+        this.setState({orders:filteredData});
 
     }
 
@@ -47,20 +47,20 @@ export class Orders extends Component <any, any> {
             }
         });
 
-        this.setState({Orders:sortedData});
+        this.setState({orders:sortedData});
     }
 
-    changeorderIdFilter = (e)=>{
-        this.state.orderIdFilter=e.target.value;
+    changeDepartmentIdFilter = (e)=>{
+        this.setState({ orderIDFilter: e.target.event.target.value })
         this.FilterFn();
     }
     changeorderTypeFilter = (e)=>{
-        this.state.orderTypeFilter=e.target.value;
+        this.setState({ orderTypeFilter: e.target.event.target.value })
         this.FilterFn();
     }
 
     refreshList(){
-        fetch(variables.API_URL+'orders')
+        fetch(variables.API_URL+'department')
         .then(response=>response.json())
         .then(data=>{
             this.setState({orders:data,ordersWithoutFilter:data});
@@ -71,34 +71,34 @@ export class Orders extends Component <any, any> {
         this.refreshList();
     }
     
-    changeorderType =(e)=>{
-        this.setState({orderType:e.target.value});
+    changeDepartmentName =(e)=>{
+        this.setState({ordertype:e.target.value});
     }
 
     addClick(){
         this.setState({
-            modalTitle:"Add Order",
-            orderId:0,
-            orderType:""
+            modalTitle:"Create Order",
+            orderID:0,
+            ordertype:""
         });
     }
     editClick(dep){
         this.setState({
-            modalTitle:"Edit Order",
-            orderId:dep.orderId,
-            orderType:dep.orderType
+            modalTitle:"Edit Department",
+            orderID:dep.orderID,
+            ordertype:dep.ordertype
         });
     }
 
     createClick(){
-        fetch(variables.API_URL+'Orders',{
+        fetch(variables.API_URL+'department',{
             method:'POST',
             headers:{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
             },
             body:JSON.stringify({
-                orderType:this.state.orderType
+                ordertype:this.state.ordertype
             })
         })
         .then(res=>res.json())
@@ -106,21 +106,21 @@ export class Orders extends Component <any, any> {
             alert(result);
             this.refreshList();
         },(error)=>{
-            alert('Ooops! this has failed');
+            alert('Failed');
         })
     }
 
 
     updateClick(){
-        fetch(variables.API_URL+'Orders',{
+        fetch(variables.API_URL+'department',{
             method:'PUT',
             headers:{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
             },
             body:JSON.stringify({
-                orderId:this.state.orderId,
-                orderType:this.state.orderType
+                orderID:this.state.orderID,
+                ordertype:this.state.ordertype
             })
         })
         .then(res=>res.json())
@@ -134,7 +134,7 @@ export class Orders extends Component <any, any> {
 
     deleteClick(id){
         if(window.confirm('Are you sure?')){
-        fetch(variables.API_URL+'Orders/'+id,{
+        fetch(variables.API_URL+'department/'+id,{
             method:'DELETE',
             headers:{
                 'Accept':'application/json',
@@ -153,10 +153,10 @@ export class Orders extends Component <any, any> {
 
     render(){
         const {
-            Orders,
+            orders,
             modalTitle,
-            orderId,
-            orderType
+            orderID,
+            ordertype
         }=this.state;
 
         return(
@@ -167,7 +167,7 @@ export class Orders extends Component <any, any> {
     data-bs-toggle="modal"
     data-bs-target="#exampleModal"
     onClick={()=>this.addClick()}>
-        Add Order
+        Create Order
     </button>
     <table className="table table-striped">
     <thead>
@@ -177,18 +177,18 @@ export class Orders extends Component <any, any> {
 
             
             <input className="form-control m-2"
-            onChange={this.changeorderIdFilter}
+            onChange={this.changeDepartmentIdFilter}
             placeholder="Filter"/>
             
             <button type="button" className="btn btn-light"
-            onClick={()=>this.sortResult('orderId',true)}>
+            onClick={()=>this.sortResult('orderID',true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
                 </svg>
             </button>
 
             <button type="button" className="btn btn-light"
-            onClick={()=>this.sortResult('orderId',false)}>
+            onClick={()=>this.sortResult('orderID',false)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
                 <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
                 </svg>
@@ -204,20 +204,20 @@ export class Orders extends Component <any, any> {
             placeholder="Filter"/>
 
             <button type="button" className="btn btn-light"
-            onClick={()=>this.sortResult('orderType',true)}>
+            onClick={()=>this.sortResult('ordertype',true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
                 </svg>
             </button>
 
             <button type="button" className="btn btn-light"
-            onClick={()=>this.sortResult('orderType',false)}>
+            onClick={()=>this.sortResult('ordertype',false)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
                 <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
                 </svg>
             </button>
             </div>
-            Order Type
+            ordertype
       
         </th>
         <th>
@@ -227,9 +227,9 @@ export class Orders extends Component <any, any> {
     </thead>
     <tbody>
         {orders.map(dep=>
-            <tr key={dep.orderId}>
-                <td>{dep.orderId}</td>
-                <td>{dep.orderType}</td>
+            <tr key={dep.orderID}>
+                <td>{dep.orderID}</td>
+                <td>{dep.ordertype}</td>
                 <td>
                 <button type="button"
                 className="btn btn-light mr-1"
@@ -244,7 +244,7 @@ export class Orders extends Component <any, any> {
 
                 <button type="button"
                 className="btn btn-light mr-1"
-                onClick={()=>this.deleteClick(dep.orderId)}>
+                onClick={()=>this.deleteClick(dep.orderID)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                     </svg>
@@ -267,20 +267,20 @@ export class Orders extends Component <any, any> {
 
    <div className="modal-body">
        <div className="input-group mb-3">
-        <span className="input-group-text">Order Type</span>
+        <span className="input-group-text">ordertype</span>
         <input type="text" className="form-control"
-        value={orderType}
-        onChange={this.changeorderType}/>
+        value={ordertype}
+        onChange={this.changeDepartmentName}/>
        </div>
 
-        {orderId==0?
+        {orderID==0?
         <button type="button"
         className="btn btn-primary float-start"
         onClick={()=>this.createClick()}
         >Create</button>
         :null}
 
-        {orderId!=0?
+        {orderID!=0?
         <button type="button"
         className="btn btn-primary float-start"
         onClick={()=>this.updateClick()}
@@ -298,3 +298,5 @@ export class Orders extends Component <any, any> {
         )
     }
 }
+
+export default Orders;
--- Frontend/src/views/Home/index.tsx
+++ Frontend/src/views/Home/index.tsx
@@ -5,21 +5,20 @@ import Header from "../../components/Header"
 import Container from '@material-ui/core/Container';
 import SearchBar from "material-ui-search-bar"
 import Box from '@mui/material/Box'
-import Table from "../../components/Table"
+import Orders from "../../components/Orders"
+
 
 export default function Home(){
     
     return <Page headerTitle={"Home"}>
         
         <Box sx={{ display: 'inline-flex'}}>
-       <SearchBar/> 
-          
-      <DropdownSelect  options={[{label: 'Bobs Burgers', value: 1}, {label: 'Wendys', value: 1},{label: 'Amazon', value: 1}, {label: 'Coca-Cola', value: 1},{label: 'Dunder Mifflin', value: 1}]} placeholder ="ok" onSelectOption ={()=>{}} value= {{label: 'Customers', value: 1}} />
-      <DropdownSelect  options={[{label: 'Standard', value: 1}, {label: 'SaleOrder', value: 1},{label: 'PurchaseOrder', value: 1}, {label: 'TransferOrder', value: 1},{label: 'ReturnOrder', value: 1}]} placeholder ="ok" onSelectOption ={()=>{}} value= {{label: 'Order Type', value: 1}} />
-      
-        </Box>
-        <Table/>
        
+          
+
+        </Box>
+        <Orders></Orders>
+        
         
     </Page>
 }
